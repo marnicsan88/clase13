f1cd5f415a15ebcc3dc7fb2aa8f9cbba3b96dfd5
marnicsan88 <marnicsan@gmail.com>
2020-09-22 22:52:57 -0300

    Tuve que subir logo y loguito a ImageShack y usar esas urls en lugar de los logos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink,
   UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText } from 'reactstrap';
-import logo from './../resources/logo.png';
-import loguito from './../resources/loguito.png';
+import logo from '../resources/logo.png';
+import loguito from '../resources/loguito.png';
 import Searchbox from './SearchBox'
 import CartIcon from './CartIcon'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -21,8 +21,8 @@ const NavPpal = (props) => {
   return (
       <Navbar color="light" light expand="md" className="nav-background flex-space-between" style={{display:displayFlex}}>
         <NavbarBrand href="/" className="text-white flexible">
-          <img src={logo} alt="Reactive Market Logo" className="d-none d-sm-block"/>
-          <img src={loguito} alt="Reactive Market Logo" className="d-block d-sm-none"/>
+          <img src="https://imagizer.imageshack.com/img922/9889/P9I7Vu.png" alt="Reactive Market Logo" className="d-none d-sm-block"/>
+          <img src="https://imagizer.imageshack.com/img923/6550/2RSH3p.png" alt="Reactive Market Logo" className="d-block d-sm-none"/>
         </NavbarBrand>
         <Searchbox categorias={props.categorias} />
         <div style={{display:displayFlex, justifyContent:"center"}}>
